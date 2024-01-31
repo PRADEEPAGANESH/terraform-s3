24685da4abfab5ce037fb63d99d88ea78a7edb95
PRADEEPAGANESH <100024448+PRADEEPAGANESH@users.noreply.github.com>
2024-01-31 09:18:53 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "The AWS region."
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+variable "bucket_acl" {
+  description = "The access control list (ACL) for the bucket."
+  default     = "private"
+}
+
+variable "versioning_enabled" {
+  description = "Whether versioning should be enabled for the bucket."
+  default     = "Enabled"
+
+}
