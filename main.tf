2d41eaad6828c99df24bfbcf1ab9270b8bd08e8e
Pradeepa G <100024448+PRADEEPAGANESH@users.noreply.github.com>
2024-03-26 12:18:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "def-buck"
+    bucket = var.bucket_name
  
 }
 resource "aws_s3_bucket_versioning" "enabled" {
